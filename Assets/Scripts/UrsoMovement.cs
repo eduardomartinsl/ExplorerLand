6a78a726e697e1736a1ff7a6671c75757dd171dc
Eduardo M <emartinslemos@gmail.com>
2017-03-17 16:30:20 -0300

    Elementos de cenario Low Poly

---
+++ Assets/Scripts/UrsoMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UrsoMovement : MonoBehaviour {
+
+	Transform player;
+	public float velocidade = 2f;
+	public float rotacao = 120f;
+	public Animator ControladorDeAnimacao;
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponent<Transform> ();
+		ControladorDeAnimacao = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float gira = Input.GetAxis ("Horizontal");
+		float frenteTras= Input.GetAxis ("Vertical");
+
+		Movimentando (frenteTras, gira);
+		Animando (frenteTras, gira);
+	}
+
+	public void Movimentando(float frenteTras, float gira){
+		player.Translate (Vector3.forward * Time.deltaTime * velocidade * frenteTras);
+		player.Rotate (0, rotacao * Time.deltaTime * gira, 0);
+	}
+
+	public void Animando(float frenteTras, float gira){
+		bool _caminhando = frenteTras != 0f || gira != 0f;
+		ControladorDeAnimacao.SetBool ("Caminhando", _caminhando);
+	}
+}
